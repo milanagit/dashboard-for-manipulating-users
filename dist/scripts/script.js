d5c7b571413d77e998ca8ed9dc9b32603b1156de
Milana <msugic@gmail.com>
2021-11-13 23:19:28 +0100

    added drag and drop functionality

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,18 +1,49 @@
-window.onload = changeView();
+const horizontalViewBtn = document.querySelectorAll('.horizontalView-btn');
+const verticalViewBtn = document.querySelectorAll('.verticalView-btn');
+const table = document.querySelectorAll('.table-users');
 
-// Change view
+const draggables = document.querySelectorAll('.table-users tr:not(.tableRow-heading)');
+
+changeView();
+dragAndDropUsers();
+
+//***** Change view *****//
 function changeView() {
-  const horizontalViewBtn = document.querySelectorAll('.horizontalView-btn');
-  const verticalViewBtn = document.querySelectorAll('.verticalView-btn');
-  const table = document.querySelectorAll('.table-users');
 
-    console.log(verticalViewBtn);
+  // Blocks
+  verticalViewBtn[0].addEventListener('click', () => {
+      table[0].classList.add('verticalView');
+  });
 
-    //verticalViewBtn.addEventListener('click', function() {
-    //    table.classList.add('verticalView');
-    //});
+  // Rows
+  horizontalViewBtn[0].addEventListener('click', () => {
+    table[0].classList.remove('verticalView');
+  });
+    
+}
 
-    verticalViewBtn.addEventListener("click", function() {
-        console.log('hello');
-      });
+//***** Drag and drop users *****//
+function dragAndDropUsers() {
+  // Dragstart and Dragend
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+      draggable.classList.add('dragging');
+    });
+
+    draggable.addEventListener('dragend', () => {
+      draggable.classList.remove('dragging');
+    });
+  });
+
+  // Drop row
+  const containers = document.querySelectorAll('.table-users tr:not(.tableRow-heading, .dragging)');
+
+  containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+      e.preventDefault();
+      const draggable = document.querySelectorAll('.dragging');
+      container.before(draggable[0]);
+    });
+  });
+  
 }
